577e840b6ac4480b67bba3f7fe69fe4f5159fbcd
Xiaofei <xiaofeifeng@nettech-global.com>
2018-06-21 20:07:08 +0800

    rename from test4_shell to shell

---
+++ shell/debug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# File name:      debug.sh
+#Author:         xiaofeifeng
+#Mail:           xiaofeifeng@nettech-global.com
+#Created Time:   2018-06-21 19:37:25 weeks:25
+
+for i in {1..6};
+do
+	echo $i
+done
+echo "The script is $0"
